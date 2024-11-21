dcfdb7009b4cf6235c635ea3e886f284740b7f61
Mehmet Ali Ayık <mehmet.ayik@digilera.com>
2024-11-21 23:07:23 +0300

    SignUp initialize

--- SampleProjectWeb/Controllers/HomeController.cs
+++ SampleProjectWeb/Controllers/HomeController.cs
@@ -1,18 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using SampleProjectWeb.Models;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
+using SampleProjectWeb.Models.ViewModels;
 
 namespace SampleProjectWeb.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController(ILogger<HomeController> logger, UserManager<IdentityUser> userManager) : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
@@ -26,6 +21,29 @@ namespace SampleProjectWeb.Controllers
         public IActionResult SignUp()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var userToCreate = new IdentityUser()
+            {
+                UserName = model.Email,
+                Email = model.Email
+            };
+
+            var result = await userManager.CreateAsync(userToCreate, model.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return RedirectToAction(nameof(SignIn));
         }
 
         public IActionResult SignIn()
---
+++ SampleProjectWeb/Models/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,7 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleProjectWeb.Models.ViewModels
+{
+    public record SignUpViewModel([Required] string Email, [Required] string Password, [Required] string ConfirmPassword);
+
+}
